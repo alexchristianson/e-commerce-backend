4116880a795afca2a8440071522e7ba9aac10f35
Alex C <alex.christianson.05@gmail.com>
2021-11-19 11:52:06 -0600

    got server up and running

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -1,5 +1,4 @@
 const router = require('express').Router();
-const { regexp } = require('sequelize/types/lib/operators');
 const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
