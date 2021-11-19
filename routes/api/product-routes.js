4124f1eeb986c85590d795d9ce7cf438cbd14fb3
Alex C <alex.christianson.05@gmail.com>
2021-11-19 11:03:42 -0600

    add product routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -5,14 +5,33 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 
 // get all products
 router.get('/', (req, res) => {
-  // find all products
-  // be sure to include its associated Category and Tag data
+  Product.findAll({
+    include: [Category, {
+      model: Tag,
+      through: ProductTag
+    }]
+  }).then(response => res.json(response))
+  .catch(err => {
+    console.log(err);
+    res.json(err);
+  });
 });
 
 // get one product
 router.get('/:id', (req, res) => {
-  // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+  Product.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: [Category, {
+      model: Tag,
+      through: ProductTag
+    }]
+  }).then(response => res.json(response))
+  .catch(err => {
+    console.log(err);
+    res.json(err);
+  });
 });
 
 // create new product
@@ -90,7 +109,15 @@ router.put('/:id', (req, res) => {
 });
 
 router.delete('/:id', (req, res) => {
-  // delete one product by its `id` value
+  Product.destroy({
+    where: {
+      id: req.params.id
+    }
+  }).then(response => res.json(response))
+  .catch(err => {
+    console.log(err);
+    res.json(err);
+  });
 });
 
 module.exports = router;
